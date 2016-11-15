fb85d5dca41083d7d19bcf574a7e8c77160737e2
g <gunther@cloudscaling.com>
2016-11-15 12:18:51 -0800

    added table display of output

---
+++ js/drawtable.js
@@ -0,0 +1,32 @@
+function createCell (text, row) {
+	var cell = document.createElement("td");
+	var cellText = document.createTextNode(text);
+	cell.appendChild(cellText);
+	row.appendChild(cell);
+}
+
+function createRow (cell1text, cell2text, tblBody) {
+	var row = document.createElement("tr");
+	createCell(cell1text, row);
+	createCell(cell2text, row);
+	tblBody.appendChild(row);
+}
+
+function drawTable (estateValue, tax, adjustedEstateValue, remainingEstate) {
+//	var body = document.getElementsByTagName("body")[0];
+	var tableHolder = document.getElementById("tableholder");
+
+	var tbl = document.createElement("table");
+	var tblBody = document.createElement("tbody");
+
+	tbl.id="taxtable";
+	tbl.className+="w3-table w3-table-all w3-bordered w3-striped";
+	createRow("Estimated estate value", accounting.formatMoney(estateValue), tblBody);
+	createRow("Tax you will pay", accounting.formatMoney(tax), tblBody);
+	createRow("Effective tax rate", effectiveRate.toFixed(2) +"%", tblBody);
+	createRow("Remaining value of estate after paying tax", accounting.formatMoney(remainingEstate), tblBody);
+
+	tbl.appendChild(tblBody);
+	tableHolder.appendChild(tbl);
+}
+
